e11c4b823ce108ec1c0ea9d993b0e5dc1c6398d9
Felipe <funck.felipe@hotmail.com>
2024-07-27 00:03:20 -0300

    Alterando script banco de dados

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,23 @@
 -- Application Schema
 create table if not exists account (
 	id serial not null primary key,
-	food_balance integer not null,
-	meal_balance integer not null,
-	cash_balance integer not null,
 	total_balance integer not null
+);
+
+create table if not exists account_balance (
+    id serial not null primary key,
+    account_id integer not null,
+    balance_type varchar not null,
+    total_balance integer not null,
+    constraint account_id_fk foreign key (account_id) references account(id)
 );
 
 create table if not exists transaction (
 	id serial not null primary key,
-	totalAmount integer not null,
+	total_amount integer not null,
 	mcc varchar not null,
 	merchant varchar not null,
-	account_id int not null,
+	account_id integer not null,
 	created_at timestamp not null,
 	constraint account_id_fk foreign key (account_id) references account(id)
 );
-
-insert into account (id, food_balance, meal_balance, cash_balance, total_balance)
-values (1, 1000, 2000, 3000, 6000),
-       (2, 500, 600, 700, 1800);
